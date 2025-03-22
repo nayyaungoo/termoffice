69799d5f357681344e806ada8052b157328e4f0e
biscuit <nayyaungoo@hotmail.com>
2025-03-22 23:31:51 +0630

    Remove the unnecessary import in termoffice/__main__.py.

--- termoffice/__main__.py
+++ termoffice/__main__.py
@@ -1,4 +1,7 @@
-from .ui import MainMenu, PathSpecifier
+from .ui import (
+    MainMenu,
+    #PathSpecifier
+)
 
 MainMenu.run()                  # passed
 MainMenu.clear()                # passed
